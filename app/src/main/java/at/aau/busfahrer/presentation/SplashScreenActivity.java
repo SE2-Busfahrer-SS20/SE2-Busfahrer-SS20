6df4df38ce60260933dac837c414a9e86d8a20e2
mmaau <m1macher@edu.aau.at>
2020-04-09 12:37:19 +0200

    Added xml app resource files and start screenActivity
    * set app screen orientation to landscape sensor
    * xml resource background for button, textView, editText, logo
    * splash screen activity when starting the app

---
+++ app/src/main/java/at/aau/busfahrer/presentation/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package at.aau.busfahrer.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import at.aau.busfahrer.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    /*
+       Start Screen when opening the App
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideAppTitleBar();
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+
+
+
+    // hide title bar and status bar for fullscreen mode
+    private void hideAppTitleBar(){
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        View decorView = getWindow().getDecorView();
+        ((View) decorView).setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+        // notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+
+
+
+
+}
