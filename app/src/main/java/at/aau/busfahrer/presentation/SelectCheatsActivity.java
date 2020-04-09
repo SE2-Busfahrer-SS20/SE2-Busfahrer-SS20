4d781ce76c3c3b4367f8d94d08594aa3aa1a05fc
mmaau <m1macher@edu.aau.at>
2020-04-09 16:10:38 +0200

    linked menu to story 06

--- app/src/main/java/at/aau/busfahrer/presentation/SelectCheatsActivity.java
+++ app/src/main/java/at/aau/busfahrer/presentation/SelectCheatsActivity.java
@@ -2,13 +2,38 @@ package at.aau.busfahrer.presentation;
 import at.aau.busfahrer.*;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class SelectCheatsActivity extends AppCompatActivity {
+
+    Button bt_start;    //This button is only for test puropose to open GuessActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideAppTitleBar();
         setContentView(R.layout.activity_select_cheats);
+
+        bt_start = findViewById(R.id.bt_start);
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open GuessActivity
+                Intent intent = new Intent(SelectCheatsActivity.this, at.aau.busfahrer.presentation.GuessActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void hideAppTitleBar(){
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
