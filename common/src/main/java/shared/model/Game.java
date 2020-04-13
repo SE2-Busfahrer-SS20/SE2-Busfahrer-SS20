d78975c73a5f871081d7e2299fbad017d72b5b4d
Gerold Auer <geroldau@edu.aau.at>
2020-04-13 23:14:14 +0200

    Refactor Game Class, add PlayerList and PlayerCount.

--- common/src/main/java/shared/model/Game.java
+++ common/src/main/java/shared/model/Game.java
@@ -1,7 +1,12 @@
 package shared.model;
 
+import java.util.List;
+
 public interface Game {
 
     GameState getState();
     void setState(GameState state);
+    int getPlayerCount();
+    List<Player> getPlayerList();
+    void setPlayerList(List<Player> playerList);
 }
--- common/src/main/java/shared/model/impl/GameImpl.java
+++ common/src/main/java/shared/model/impl/GameImpl.java
@@ -1,13 +1,22 @@
 package shared.model.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import shared.model.Game;
 import shared.model.GameState;
+import shared.model.Player;
 
 public class GameImpl implements Game {
 
     private GameState state;
+    private int gameCount;
+    private List<Player> playerList;
 
-    public GameImpl() {
+
+
+    public GameImpl(int gameCount) {
+        this.playerList = new ArrayList<>(gameCount);
         this.state = GameState.INIT;
     }
 
@@ -19,6 +28,19 @@ public class GameImpl implements Game {
     @Override
     public void setState(GameState state) {
         this.state = state;
+    }
+
+    @Override
+    public int getPlayerCount() {
+        return gameCount;
+    }
+
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(List<Player> playerList) {
+        this.playerList = playerList;
     }
 
 }
