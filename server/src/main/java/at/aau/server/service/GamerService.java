fe933d39e1ed694f46f13f19917a3cc6bbd05b4f
Gerold Auer <geroldau@edu.aau.at>
2020-04-13 19:09:18 +0200

    add Interface for GameService

---
+++ server/src/main/java/at/aau/server/service/GamerService.java
@@ -0,0 +1,37 @@
+package at.aau.server.service;
+
+import java.util.List;
+
+import shared.model.GameState;
+import shared.model.Player;
+
+public interface GamerService {
+    /**
+     * Returns List of registered Players.
+     * @return List<Player>
+     */
+    List<Player> getPlayerList();
+
+    /**
+     * Add Player to list.
+     * @param player
+     * @return boolean status,  false in case of a full List.
+     */
+    boolean addPlayer(Player player);
+
+    /**
+     *
+     * @return GameState
+     */
+    GameState getGameState();
+
+    /**
+     *
+     * @return count of Players.
+     */
+    int getPlayerCount();
+    boolean gameReady();
+    void nextLab();
+    void startGame();
+    void endGame();
+}
