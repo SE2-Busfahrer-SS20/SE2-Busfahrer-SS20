eed2215f2a08d68759c86fcc8b13164bb88b8593
Gerold Auer <geroldau@edu.aau.at>
2020-04-13 19:22:14 +0200

    add basic methods for Runnable.

--- server/src/main/java/at/aau/server/GameServer.java
+++ server/src/main/java/at/aau/server/GameServer.java
@@ -1,4 +1,61 @@
 package at.aau.server;
 
-public class GameServer {
+import java.io.IOException;
+
+import at.aau.server.service.GameService;
+import at.aau.server.service.impl.GameServiceImpl;
+import shared.model.impl.PlayerImpl;
+import shared.networking.NetworkServer;
+import shared.networking.dto.BaseMessage;
+import shared.networking.dto.TextMessage;
+import shared.networking.kryonet.NetworkServerKryo;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+
+public class GameServer extends NetworkServerKryo implements Runnable{
+
+    // TODO: delete before deploy.
+    private final static int MOCK_COUNT = 1;
+
+    private Thread thread;
+    private NetworkServer server;
+    private GameService gameService;
+
+    public GameServer() {
+        server = new NetworkServerKryo();
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    // TODO: extract Listener to Listener Factory.
+    @Override
+    public void start() throws IOException {
+        super.start();
+        super.addListener(new Listener() {
+            public void received(Connection connection, Object object) {
+                if (object instanceof BaseMessage) {
+                    if (((TextMessage) object).text.equals("createGame")) {
+                        // TODO: implement correct palyer count.
+                        gameService = new GameServiceImpl(MOCK_COUNT);
+                        System.out.println("Game created");
+                    } else if (((TextMessage) object).text.equals("registerGame")) {
+                        gameService.addPlayer(new PlayerImpl());
+                        System.out.println("Player registered.");
+                    }
+                } else {
+                    // TODO: implement.
+                }
+
+            }
+        });
+
+        thread = new Thread(this);
+        thread.start();
+    }
+
 }
