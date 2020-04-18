6f701c4dadc459ec23a0c85f7d15606fb4ca833f
Gerold Auer <geroldau@edu.aau.at>
2020-04-18 19:56:10 +0200

    Implement server connection logic.

--- app/src/main/java/at/aau/busfahrer/service/impl/GameService.java
+++
@@ -1,4 +0,0 @@
-package at.aau.busfahrer.service.impl;
-
-public class GameService {
-}
---
+++ app/src/main/java/at/aau/busfahrer/service/impl/GameServiceImpl.java
@@ -0,0 +1,27 @@
+package at.aau.busfahrer.service.impl;
+
+
+import at.aau.busfahrer.presentation.GuessActivity;
+import at.aau.busfahrer.service.GameService;
+import shared.model.Game;
+import shared.networking.NetworkClient;
+import shared.networking.dto.CreateGameMessage;
+import shared.networking.kryonet.NetworkClientKryo;
+
+public class GameServiceImpl implements GameService {
+
+    private NetworkClient client;
+
+    public GameServiceImpl(String host) {
+
+        this.client = new NetworkClientKryo(host);
+        client.start();
+    }
+
+
+    public void createGame(int playercount, String gameName) {
+
+        // TODO: implement Create Game.
+
+    }
+}
