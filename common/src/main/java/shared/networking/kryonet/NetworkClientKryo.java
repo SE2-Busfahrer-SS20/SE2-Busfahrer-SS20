e0ccc2cae229859d0b547f7d4b87218b6e897392
Gerold Auer <gerold.auer@live.at>
2020-05-05 12:59:06 +0200

    Change registerCallback method to register multiple Callbacks. Add HashMap for Callback management.

--- common/src/main/java/shared/networking/kryonet/NetworkClientKryo.java
+++ common/src/main/java/shared/networking/kryonet/NetworkClientKryo.java
@@ -4,7 +4,10 @@ import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.minlog.Log;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import shared.model.GameState;
 import shared.model.impl.playersStorage;
@@ -14,22 +17,26 @@ import shared.networking.dto.BaseMessage;
 import shared.networking.dto.ConfirmRegisterMessage;
 import shared.networking.dto.NewPlayerMessage;
 import shared.networking.dto.StartGameMessage;
+import shared.networking.dto.StartPLabMessage;
 import shared.networking.dto.TextMessage;
 
 import static shared.networking.kryonet.NetworkConstants.CLASS_LIST;
 
 public class NetworkClientKryo implements NetworkClient, KryoNetComponent {
+
     private Client client;
-    private Callback<BaseMessage> callback;
+    // Callback Map to store callbacks for every DTO Class. HashMap to access object in O(1).
+    private Map<Class, Callback<BaseMessage>> callbackMap = new HashMap<>();
+    private static NetworkClient instance;
 
-
-    public NetworkClientKryo() {
+    private NetworkClientKryo() {
         client = new Client();
         registerClasses();
     }
+
     public void registerClass(Class c) {
-         client.getKryo().register(c);
-   }
+        client.getKryo().register(c);
+    }
 
 
     @Override
@@ -40,40 +47,35 @@ public class NetworkClientKryo implements NetworkClient, KryoNetComponent {
 
         client.addListener(new Listener() {
             public void received(Connection connection, Object object) {
-
-                if(object instanceof ConfirmRegisterMessage){
+                // handle null objects or not known Objects.
+                if (!(object instanceof BaseMessage)) {
+                    Log.info("Network Client Listener Error: Received Object is null or not from Type BaseMessage.");
+                } else if (object instanceof ConfirmRegisterMessage) {
                     Log.debug("Registration Confirmed");
-                    playersStorage.setMaster(((ConfirmRegisterMessage)object).isMaster());
-                    playersStorage.setCards(((ConfirmRegisterMessage)object).getCards());
-                }
-
-                if(object instanceof NewPlayerMessage){
+                    playersStorage.setMaster(((ConfirmRegisterMessage) object).isMaster());
+                    playersStorage.setCards(((ConfirmRegisterMessage) object).getCards());
+                } else if (object instanceof NewPlayerMessage) {
                     Log.debug("New Player in the Game");
-                    playersStorage.addPlayerName(((NewPlayerMessage)object).getPlayerName());
-                    System.out.println("NEW PLAYER IN THE GAME ");
-                }
-
-                if(object instanceof StartGameMessage){
+                    playersStorage.addPlayerName(((NewPlayerMessage) object).getPlayerName());
+                    Log.info("NEW PLAYER IN THE GAME ");
+                } else if (object instanceof StartGameMessage) {
                     Log.debug("Game can start now");
                     playersStorage.setState(GameState.READY);
-                    System.out.println("received SGM in listener...");
-                }
-
-
-                if (callback != null && object instanceof BaseMessage) {
-                    callback.callback((BaseMessage) object);
-                    Log.debug("Callback is instance of BaseMessage");
-                    if(object instanceof TextMessage){
-                        Log.debug(((TextMessage)(object)).getText());
-                    }
-
+                    Log.info("received SGM in listener...");
+                } else if (object instanceof StartPLabMessage) {
+                    Log.info("StartPlaBMesssage received");
+                    // call the correct callback to store cards and update UI Thread.
+                    callbackMap.get(StartPLabMessage.class).callback((BaseMessage) object);
+                } else if (object instanceof TextMessage) {
+                    Log.debug("Callback is instance of TextMessage");
+                    Log.debug(((TextMessage) (object)).getText());
                 }
             }
         });
     }
 
-    public void registerCallback(Callback<BaseMessage> callback) {
-        this.callback = callback;
+    public void registerCallback(Class dtoClass, Callback<BaseMessage> callback) {
+        this.callbackMap.put(dtoClass, callback);
     }
 
     public void sendMessage(BaseMessage message) {
@@ -85,4 +87,9 @@ public class NetworkClientKryo implements NetworkClient, KryoNetComponent {
             registerClass(c);
     }
 
+    public static synchronized NetworkClient getInstance() {
+        if (instance == null)
+            return new NetworkClientKryo();
+        return instance;
+    }
 }
