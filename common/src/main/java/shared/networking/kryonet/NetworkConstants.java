ef55621a081264da5532490e17a148cdb0c446ff
Gerold Auer <gerold.auer@live.at>
2020-05-11 17:10:53 +0200

    Add classes to ClassList Array for Serializer.

--- common/src/main/java/shared/networking/kryonet/NetworkConstants.java
+++ common/src/main/java/shared/networking/kryonet/NetworkConstants.java
@@ -1,19 +1,13 @@
 package shared.networking.kryonet;
 
-import shared.networking.dto.BaseMessage;
-import shared.networking.dto.ConfirmRegisterMessage;
-import shared.networking.dto.CreateGameMessage;
-import shared.networking.dto.NewPlayerMessage;
-import shared.networking.dto.RegisterMessage;
-import shared.networking.dto.ServerActionResponse;
-import shared.networking.dto.StartGameMessage;
-import shared.networking.dto.TextMessage;
+import shared.model.impl.PlayerImpl;
+import shared.networking.dto.*;
 
 public class NetworkConstants {
 
     public static final int TCP_PORT = 54555;
     // public static final int UDP_PORT = 54777;
-    public static final String host ="10.0.0.16";
+    public static final String host ="192.168.0.115";
 
     // List for registered dto classes. Add needed classes to the array.
     public static final Class[] CLASS_LIST = {
@@ -25,9 +19,12 @@ public class NetworkConstants {
             ConfirmRegisterMessage.class,
             NewPlayerMessage.class,
             StartGameMessage.class,
-
+            StartPLabMessage.class,
             shared.model.impl.CardImpl[].class,
-            shared.model.impl.CardImpl.class
+            shared.model.impl.CardImpl.class,
+            java.util.ArrayList.class,
+            PlayerImpl.class,
+            com.esotericsoftware.kryonet.Connection.class
 
     };
 
