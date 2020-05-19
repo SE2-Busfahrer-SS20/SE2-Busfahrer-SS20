e1cff399a9704fc78bcfe2f3375dad96e14930b9
Phil <phfreislich@edu.aau.at>
2020-05-19 14:34:33 +0200

    added Listview to ScoreFragment

--- app/src/main/java/at/aau/busfahrer/presentation/ScoreFragment.java
+++ app/src/main/java/at/aau/busfahrer/presentation/ScoreFragment.java
@@ -1,17 +1,22 @@
 package at.aau.busfahrer.presentation;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import at.aau.busfahrer.R;
+import shared.model.impl.PlayersStorageImpl;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScoreFragment extends Fragment {
+
+    private PlayersStorageImpl playersStorage = PlayersStorageImpl.getInstance();
 
     public ScoreFragment() {
         // Required empty public constructor
@@ -21,7 +26,22 @@ public class ScoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_score, container, false);
+        System.out.println("\n\n\n"+playersStorage.getPlayerNames()+"\n\n\n");
+        String[] scoreItems= new String[playersStorage.getPlayerNames().size()];
+        for(int i=0;i<scoreItems.length;i++){
+            scoreItems[i]=playersStorage.getPlayerName(i)+": "+playersStorage.getScore().get(i);
+        }
+
+        ListView listView= (ListView) view.findViewById(R.id.scoreList);
+
+        ArrayAdapter<String> listViewAdapter= new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                scoreItems
+        );
+        listView.setAdapter(listViewAdapter);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        return view;
     }
 }
