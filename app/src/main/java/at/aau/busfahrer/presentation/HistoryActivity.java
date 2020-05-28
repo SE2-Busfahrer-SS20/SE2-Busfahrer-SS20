d96bf85ad25335f77ddde48aa84147c83624ecdf
Phil <phfreislich@edu.aau.at>
2020-05-28 23:41:19 +0200

    added Tab-View to Leaderboard

--- app/src/main/java/at/aau/busfahrer/presentation/HistoryActivity.java
+++ app/src/main/java/at/aau/busfahrer/presentation/HistoryActivity.java
@@ -1,8 +1,11 @@
 package at.aau.busfahrer.presentation;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
 import at.aau.busfahrer.R;
+
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
     }
 }
