aef8882bfb737c172de461c0d99860dfe51fb53f
Phil <phfreislich@edu.aau.at>
2020-05-29 13:48:09 +0200

    added simple Prefilled History-List

--- app/src/main/java/at/aau/busfahrer/presentation/HistoryActivity.java
+++ app/src/main/java/at/aau/busfahrer/presentation/HistoryActivity.java
@@ -1,6 +1,8 @@
 package at.aau.busfahrer.presentation;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.viewpager.widget.ViewPager;
@@ -18,5 +20,15 @@ public class HistoryActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+        String[] scoreItems= new String[]{"Philipp: 10","Larissa: 9","Markus: 8","Elias: 7","Volte: 6","Gery: 10"};
+
+        ListView listView= (ListView) decorView.findViewById(R.id.historyList);
+
+        ArrayAdapter<String> listViewAdapter= new ArrayAdapter<String>(
+                getApplication(),
+                android.R.layout.simple_list_item_1,
+                scoreItems
+        );
+        listView.setAdapter(listViewAdapter);
     }
 }
