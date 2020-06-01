71a6bf12536cae694105e0c86cacf3b01e16fa88
Phil <phfreislich@edu.aau.at>
2020-06-01 11:18:39 +0200

    updated Interface

--- app/src/main/java/at/aau/busfahrer/service/LeaderboardService.java
+++ app/src/main/java/at/aau/busfahrer/service/LeaderboardService.java
@@ -6,7 +6,9 @@ import shared.networking.Callback;
 import java.util.List;
 
 public interface LeaderboardService {
-    public void updateScoreList();
+    void updateScoreList();
     void registerPlayerListCallback(Callback<List<PlayerDTO>> callback);
-    public void connect();
+    void connect();
+    String getHostname();
+    void setHostname(String hostname);
 }
