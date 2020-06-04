d836292ff2dd3335830e29079c6a825b1188d950
Gerold Auer <gerold.auer@live.at>
2020-06-04 21:18:59 +0200

    extend tests to check dtos functionality.

--- common/src/test/java/at/aau/common/shared/networking/dto/PLapDtoServiceTest.java
+++ common/src/test/java/at/aau/common/shared/networking/dto/PLapDtoServiceTest.java
@@ -1,6 +1,41 @@
 package at.aau.common.shared.networking.dto;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import shared.networking.dto.BaseMessage;
+import shared.networking.dto.DealPointsMessage;
+import shared.networking.dto.StartPLabMessage;
+import shared.networking.dto.WinnerLooserMessage;
+
 public class PLapDtoServiceTest {
 
+    private WinnerLooserMessage winnerLooserMessage;
+    private DealPointsMessage dealPointsMessage;
+    private StartPLabMessage startPLabMessage;
 
+    @Before
+    public void setup() {
+        this.winnerLooserMessage = new WinnerLooserMessage();
+        this.dealPointsMessage = new DealPointsMessage();
+        this.startPLabMessage = new StartPLabMessage();
+    }
+
+    /**
+     * check if the used DTOs are extends the BaseMessage.
+     */
+    @Test
+    public void testInheritance() {
+        Assert.assertTrue(winnerLooserMessage instanceof BaseMessage);
+        Assert.assertTrue(dealPointsMessage instanceof BaseMessage);
+        Assert.assertTrue(startPLabMessage instanceof BaseMessage);
+    }
+
+    @Test
+    public void testWinnerLooserGetterSetter() {
+        winnerLooserMessage.setIsLooser(true);
+        Assert.assertTrue(winnerLooserMessage.getIsLooser());
+        winnerLooserMessage.setIsLooser(false);
+        Assert.assertFalse(winnerLooserMessage.getIsLooser());
+    }
 }
