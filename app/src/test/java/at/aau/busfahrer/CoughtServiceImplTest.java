8f2039ee29ceb6fa6379c9540524d93d2b5532f7
Larissa Krainer <lakrainer@edu.aau.at>
2020-06-04 11:59:00 +0200

    Added test for isCheating = true.

--- app/src/test/java/at/aau/busfahrer/CoughtServiceImplTest.java
+++ app/src/test/java/at/aau/busfahrer/CoughtServiceImplTest.java
@@ -7,13 +7,25 @@ import org.junit.Test;
 
 import at.aau.busfahrer.service.CoughtService;
 import at.aau.busfahrer.service.impl.CoughtServiceImpl;
+import shared.model.PlayersStorage;
+import shared.model.impl.PlayerDTOImpl;
+import shared.model.impl.PlayersStorageImpl;
 
 public class CoughtServiceImplTest {
     private CoughtService coughtService;
+    private PlayersStorageImpl playersStorage;
+    private PlayerDTOImpl player1;
+    private PlayerDTOImpl player2;
 
     @Before
     public void setup() {
         this.coughtService = CoughtServiceImpl.getInstance();
+        this.playersStorage = PlayersStorageImpl.getInstance();
+        this.player1 = new PlayerDTOImpl("Larissa",3,true);
+        this.player2 = new PlayerDTOImpl("Cought",5,false);
+        playersStorage.addPlayer(player1);
+        playersStorage.addPlayer(player2);
+
     }
     @After
     public void destroy() {
@@ -23,5 +35,12 @@ public class CoughtServiceImplTest {
     @Test
     public void testSingletonPattern() {
         Assert.assertEquals(CoughtServiceImpl.getInstance(), coughtService);
+    }
+
+    @Test
+    public void testIsCheatingTrue(){
+        playersStorage.setCurrentTurn(0);
+        playersStorage.setTempID(1);
+        Assert.assertEquals(true,coughtService.isCheating());
     }
 }
