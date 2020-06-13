59003cd4eafe9a8cc34da530f81604a6c2c8772a
Larissa Krainer <lakrainer@edu.aau.at>
2020-06-13 19:00:42 +0200

    Added Testclass and Tests for the DTO NewPlayerMessage.

---
+++ common/src/test/java/at/aau/common/shared/networking/dto/NewPlayerMessageDToTest.java
@@ -0,0 +1,40 @@
+package at.aau.common.shared.networking.dto;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import shared.model.PlayerDTO;
+import shared.model.impl.PlayerDTOImpl;
+import shared.networking.dto.BaseMessage;
+import shared.networking.dto.NewPlayerMessage;
+
+public class NewPlayerMessageDToTest {
+    private NewPlayerMessage newPlayerMessage;
+    private PlayerDTO playerDTO;
+    private PlayerDTO playerTest;
+
+    @Before
+    public void setup(){
+        this.newPlayerMessage = new NewPlayerMessage();
+        this.playerDTO = new PlayerDTOImpl("Larissa",30,false);
+        this.playerTest = new PlayerDTOImpl("Lars",12,true);
+    }
+
+    @Test
+    public void testInheritance() {
+        Assert.assertTrue(newPlayerMessage instanceof BaseMessage);
+    }
+    @Test
+    public void constrcutorTest(){
+
+        newPlayerMessage = new NewPlayerMessage(playerDTO);
+        Assert.assertEquals(playerDTO,newPlayerMessage.getPlayer());
+    }
+    @Test
+    public void gettersetterTest(){
+        newPlayerMessage = new NewPlayerMessage(playerDTO);
+        newPlayerMessage.setPlayer(playerTest);
+        Assert.assertEquals(playerTest,newPlayerMessage.getPlayer());
+    }
+}
