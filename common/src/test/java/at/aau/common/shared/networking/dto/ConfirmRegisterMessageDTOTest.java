177b9b5fc72a0acc07951cc34e1c4642690dc0d8
Larissa Krainer <lakrainer@edu.aau.at>
2020-06-13 18:34:19 +0200

    Finished Tests for DTO ConfirmRegisterMessage.

--- common/src/test/java/at/aau/common/shared/networking/dto/ConfirmRegisterMessageDTOTest.java
+++ common/src/test/java/at/aau/common/shared/networking/dto/ConfirmRegisterMessageDTOTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 
 import shared.model.Card;
 import shared.model.Deck;
+import shared.model.Player;
 import shared.model.impl.CardImpl;
 import shared.model.impl.DeckImpl;
+import shared.model.impl.PlayerImpl;
 import shared.networking.dto.BaseMessage;
 import shared.networking.dto.ConfirmRegisterMessage;
 
@@ -16,8 +18,7 @@ public class ConfirmRegisterMessageDTOTest {
     private ConfirmRegisterMessage confirmRegisterMessage;
     private Deck deck;
     private Card[] cards;
-
-
+    private Player player;
 
     @Before
     public void setup(){
@@ -27,9 +28,58 @@ public class ConfirmRegisterMessageDTOTest {
             cards[i] = deck.drawCard();
         }
         this.confirmRegisterMessage = new ConfirmRegisterMessage(cards);
+        this.player = new PlayerImpl("Hansi","TestAdresse",cards,null);
+        player.setTempID(2);
     }
     @Test
     public void testInheritance() {
         Assert.assertTrue(confirmRegisterMessage instanceof BaseMessage);
+    }
+    @Test
+    public void getCardsTest(){
+        Assert.assertEquals(cards,confirmRegisterMessage.getCards());
+    }
+    @Test
+    public void setCardsTest(){
+        confirmRegisterMessage.setCards(cards);
+        Assert.assertEquals(cards,confirmRegisterMessage.getCards());
+    }
+    @Test
+    public void newDTOwithParameterPlayerCardTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player);
+        Assert.assertEquals(cards,confirmRegisterMessage.getCards());
+    }
+    @Test
+    public void newDTOwithParameterPlayergetIDTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player);
+        Assert.assertEquals(2,confirmRegisterMessage.getID());
+    }
+    @Test
+    public void newDTOwithParameterPlayersetIDTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player);
+        confirmRegisterMessage.setID(1);
+        Assert.assertEquals(1,confirmRegisterMessage.getID());
+    }
+    @Test
+    public void newDTOwithParameterPlayerMastergetIDTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player,true);
+        Assert.assertEquals(2,confirmRegisterMessage.getID());
+    }
+    @Test
+    public void newDTOwithParameterPlayerMastersetIDTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player,true);
+        confirmRegisterMessage.setID(1);
+        Assert.assertEquals(1,confirmRegisterMessage.getID());
+    }
+    @Test
+    public void newDTOwithParameterPlayerMastersetMasterTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player,true);
+        confirmRegisterMessage.setMaster(false);
+        Assert.assertEquals(false,confirmRegisterMessage.isMaster());
+    }
+    @Test
+    public void newDTOwithParameterPlayerMastergetMasterTest(){
+        confirmRegisterMessage = new ConfirmRegisterMessage(player,true);
+        Assert.assertEquals(true,confirmRegisterMessage.isMaster());
     }
 }
