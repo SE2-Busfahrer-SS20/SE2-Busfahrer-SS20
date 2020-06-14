2e71ee1e48d6717c36ba27ca46f41842d248e4c8
Larissa Krainer <lakrainer@edu.aau.at>
2020-06-14 08:45:22 +0200

    Added tests for PlayerDTIImplTest.

--- common/src/test/java/shared/model/impl/PlayerDTOImplTest.java
+++ common/src/test/java/shared/model/impl/PlayerDTOImplTest.java
@@ -1,5 +1,6 @@
 package shared.model.impl;
 
+import org.junit.Assert;
 import org.junit.Test;
 import shared.model.Player;
 import shared.model.PlayerDTO;
@@ -87,5 +88,27 @@ public class PlayerDTOImplTest {
         assertEquals(expected.getName(), actual.getName());
         assertEquals(expected.getScore(), actual.getScore());
         assertEquals(expected.isCheating(), actual.isCheating());
+    }
+    @Test
+    public void constrcutorTestAndGetterisBusdriver(){
+        PlayerDTO playerDTO = new PlayerDTOImpl("Lars",23,true,"Test");
+        Assert.assertEquals(false,playerDTO.isBusdriver());
+    }
+    @Test
+    public void getterBusdriverTest(){
+        PlayerDTO playerDTO = new PlayerDTOImpl("Lars",23,true,"Test");
+        playerDTO.setBusdriver();
+        Assert.assertEquals(true,playerDTO.isBusdriver());
+    }
+    @Test
+    public void constrcutorTestAndGetterMac(){
+        PlayerDTO playerDTO = new PlayerDTOImpl("Lars",23,true,"Test");
+        Assert.assertEquals("Test",playerDTO.getMAC());
+    }
+    @Test
+    public void getterMacTest(){
+        PlayerDTO playerDTO = new PlayerDTOImpl("Lars",23,true,"Test");
+        playerDTO.setMAC("NeuerTest");
+        Assert.assertEquals("NeuerTest",playerDTO.getMAC());
     }
 }
